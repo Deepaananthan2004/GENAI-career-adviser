74f3e6383c6dd629ed36c56dbb43aab2a233f9b0
Deepa ananthan AR <deepaananthan1.8@gmail.com>
2025-10-15 20:18:37 +0530

    Delete AI career adviser directory

--- AI career adviser/firebase_config.py
+++
@@ -1,9 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-def store_to_firestore(collection, data):
-    db.collection(collection).add(data)
